cbfee2a6e3ee8e32f697d8afc2822a5c0cbdd76a
snp <gosu.snp@gmail.com>
2022-06-12 11:25:06 -0700

    Normalize speed range
    Use 0-100 instead of 0-255

--- src/fans.js
+++ src/fans.js
@@ -2,30 +2,45 @@
 
 const { Motor } = require("johnny-five");
 
+const Fan = function(motor) {
+    this.motor = motor;
+}
+
+function convertSpeed(speed) {
+    if (speed < 0 || speed > 100) {
+        throw new Error(`Invalid speed "${speed}", valid range [0, 100]`);
+    }
+
+    return speed * 2.55;
+}
+
+Fan.prototype.start = function(speed) {
+    const actualSpeed = convertSpeed(speed);
+    if (this.motor) {
+        this.motor.start(actualSpeed);
+    }
+}
+
 const Fans = function(board) {
     this.board = board;
-    this.motorA = null;
-    this.motorB = null;
+    this.fanA = null;
+    this.fanB = null;
 
     this.board.on("ready", () => this.onBoardReady());
 }
 
 Fans.prototype.start = function(speed) {
-    if (this.motorA) {
-        this.motorA.start(speed);
-    }
-    if (this.motorB) {
-        this.motorB.start(speed);
-    }
+    this.fanA.start(speed);
+    this.fanB.start(speed);
 }
 
 Fans.prototype.onBoardReady = function() {
-    this.motorA = new Motor(Motor.SHIELD_CONFIGS.ARDUINO_MOTOR_SHIELD_R3_1.A);
-    this.motorB = new Motor(Motor.SHIELD_CONFIGS.ARDUINO_MOTOR_SHIELD_R3_1.B);
+    this.fanA = new Fan(new Motor(Motor.SHIELD_CONFIGS.ARDUINO_MOTOR_SHIELD_R3_1.A));
+    this.fanB = new Fan(new Motor(Motor.SHIELD_CONFIGS.ARDUINO_MOTOR_SHIELD_R3_1.B));
 
     this.board.repl.inject({
-        motorA: this.motorA,
-        motorB: this.motorB,
+        fanA: this.fanA,
+        fanB: this.fanB,
         fans: this,
     })
 }
